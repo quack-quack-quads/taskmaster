1c5067d374ddab6cf11a74160a60346dad46d4d4
Subhajeet Lahiri <subhajeetlahiri@gmail.com>
2023-02-11 01:21:28 +0530

    auth added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,33 @@
-const handleLogin = async (req, res) => {
-    // implement here
-    res.send('Login')
+const { createUserWithEmailAndPassword, signInWithEmailAndPassword } = require("firebase/auth");
+const { ref, set, get, push, child} = require("firebase/database");
+const { db, auth } = require("../firebase-config")
+
+const signUp = async (req, res) => {
+    createUserWithEmailAndPassword(auth, req.body.email, req.body.password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        res.send(errorMessage);
+    });
+}
+
+const signIn = async (req, res)=>{
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
 }
 
 module.exports = {
-    handleLogin
+    signUp,
+    signIn
 }
--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -2,14 +2,18 @@ const { ref, set, get, push, child} = require("firebase/database");
 const { db } = require("../firebase-config")
 
 const getJobById = async (req, res) =>{
-    const dbRef = ref(db);
-    get(child(dbRef, `chats/${req.body["id"]}`)).then((snapshot) => {
-        if (snapshot.exists()) {
-            console.log(snapshot.val());
-        } else {
-            console.log("No data available");
-        }
-    }).catch((error) => {
-        console.error(error);
-    });
+    // const dbRef = ref(db);
+    // get(child(dbRef, `chats/${req.body["id"]}`)).then((snapshot) => {
+    //     if (snapshot.exists()) {
+    //         console.log(snapshot.val());
+    //     } else {
+    //         console.log("No data available");
+    //     }
+    // }).catch((error) => {
+    //     console.error(error);
+    // });
+}
+
+module.exports = {
+
 }
