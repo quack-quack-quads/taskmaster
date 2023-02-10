6eb34fe59bbc2a67694e3bd1a3ae1eba0170c6ad
Subhajeet Lahiri <subhajeetlahiri@gmail.com>
2023-02-11 02:21:19 +0530

    client functions written

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,11 +2,20 @@ const { createUserWithEmailAndPassword, signInWithEmailAndPassword } = require("
 const { ref, set, get, push, child} = require("firebase/database");
 const { db, auth } = require("../firebase-config")
 
+
+const createClient = (uid, data)=>{
+    const dbRef = ref(db);
+    data["password"] = null;
+    const newClientKey = push(child(dbRef, 'clients')).key;
+    set(ref(db, `clients/${uid}`), data);
+}
+
 const signUp = async (req, res) => {
     createUserWithEmailAndPassword(auth, req.body.email, req.body.password)
     .then((userCredential) => {
         const user = userCredential.user;
-        console.log(user);
+        createClient(user.uid, req.body);
+        res.send(user);
     })
     .catch((error) => {
         const errorCode = error.code;
@@ -16,18 +25,33 @@ const signUp = async (req, res) => {
 }
 
 const signIn = async (req, res)=>{
-    signInWithEmailAndPassword(auth, email, password)
+    signInWithEmailAndPassword(auth, req.body.email, req.body.password)
     .then((userCredential) => {
         const user = userCredential.user;
-        console.log(user);
+        res.send(user);
     })
     .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
+        res.send(errorMessage);
+    });
+}
+
+const getClient = async (req, res)=>{
+    const dbRef = ref(db);
+    get(child(dbRef, `clients/${req.body["uid"]}`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            res.send(snapshot.val())
+        } else {
+            res.send("No data available");
+        }
+    }).catch((error) => {
+        res.send(error);
     });
 }
 
 module.exports = {
     signUp,
-    signIn
+    signIn,
+    getClient
 }
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { signUp } = require("../controllers/authController")
+const { signUp, signIn, getClient } = require("../controllers/authController")
 
 router.route('/signup').post(signUp)
+router.route('/signin').post(signIn)
+router.route('/getclient').post(getClient)
 
 module.exports = router;
 
