cab031de68fc385bc191babdc7830d2f05901179
Subhajeet Lahiri <subhajeetlahiri@gmail.com>
2023-02-11 02:38:59 +0530

    jobs done

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,19 +1,40 @@
 const { ref, set, get, push, child} = require("firebase/database");
 const { db } = require("../firebase-config")
 
-const getJobById = async (req, res) =>{
-    // const dbRef = ref(db);
-    // get(child(dbRef, `chats/${req.body["id"]}`)).then((snapshot) => {
-    //     if (snapshot.exists()) {
-    //         console.log(snapshot.val());
-    //     } else {
-    //         console.log("No data available");
-    //     }
-    // }).catch((error) => {
-    //     console.error(error);
-    // });
+const addJob = async (req, res) =>{
+    const dbRef = ref(db);
+
+    const uid = req.body["uid"];
+    const data = req.body['job'];
+
+    const newJobKey = push(child(dbRef, `jobs/${uid}`)).key;
+    set(ref(db, `jobs/${uid}/${newJobKey}`), data).then(
+        ()=>{
+            res.send(newJobKey);
+        }
+    ).catch(
+        (error)=>{
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            res.send(errorMessage);
+        }
+    )
+}
+
+const getJobs = async (req, res)=>{
+    const dbRef = ref(db);
+    get(child(dbRef, `jobs/${req.body["uid"]}`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            res.send(snapshot.val())
+        } else {
+            res.send("No data available");
+        }
+    }).catch((error) => {
+        res.send(error);
+    });
 }
 
 module.exports = {
-
+    addJob,
+    getJobs
 }
--- server/routes/jobRouter.js
+++ server/routes/jobRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const {addJob, getJobs} = require('../controllers/jobController')
 
-
+router.route('/add').post(addJob)
+router.route('/get').post(getJobs)
 
 module.exports = router;
