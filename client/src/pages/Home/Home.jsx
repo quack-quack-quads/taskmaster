60ca04573a25410e54b9a6a9087dc3caa3635fd4
Sai Madhavan G <g.saimadhavan@gmail.com>
2023-02-11 17:37:59 +0530

    Work on homepage halfway done

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import "./Home.scss";
 import ChatWindow from "../../components/ChatWindow/ChatWindow";
 import Hero from "../../components/Hero/Hero";
+import CategoryCards from "../../components/CategoryCards/CategoryCards";
+import Features from "../../components/Features/Features";
 
 const Home = () => {
   return (
     <div className="Home">
       {/* <ChatWindow /> */}
       <Hero />
+      <CategoryCards />
+      <Features />
     </div>
   );
 };
