8f4b5aeacb598a040efe12ba77912ad46b46bed8
Sai Madhavan G <g.saimadhavan@gmail.com>
2023-02-11 14:06:09 +0530

    Hero component done

---
+++ client/src/screens/LandingPage.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero/Hero";
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <Hero />
+    </div>
+  );
+};
+
+export default LandingPage;
