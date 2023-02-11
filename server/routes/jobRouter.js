a123d7d55f9c711a8a9d0eace7cc50a674d3e84f
RohitShah1706 <rlshah03@gmail.com>
2023-02-11 17:13:04 +0530

    added distance query

--- server/routes/jobRouter.js
+++ server/routes/jobRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {addJob, getJobs, getJobsByCategory} = require('../controllers/jobController')
+const {addJob, getJobs, getJobsByCategory, getJobsByDistance} = require('../controllers/jobController')
 
 router.route('/add').post(addJob)
 router.route('/get').post(getJobs)
 router.route("/getByCategory").post(getJobsByCategory)
+router.route("/getByDistance").post(getJobsByDistance)
 
 module.exports = router;
